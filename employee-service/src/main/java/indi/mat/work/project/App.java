e99b0831212d3fd83de26cfaba7798330f1ac2f6
yangju90 <yangju90@163.com>
2021-08-06 00:14:21 +0800

    创建基础架子

---
+++ employee-service/src/main/java/indi/mat/work/project/App.java
@@ -0,0 +1,20 @@
+package indi.mat.work.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * Hello world!
+ *
+ */
+
+@EnableCaching
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ employee-service/src/main/java/indi/mat/work/project/controller/UserController.java
@@ -0,0 +1,26 @@
+package indi.mat.work.project.controller;
+
+import indi.mat.work.project.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/getById")
+    public Object getById(Integer id) {
+        return userService.getById(id);
+    }
+
+    @RequestMapping("/delete")
+    public Object delete(Integer id) {
+        userService.delete(id);
+        return "删除成功！";
+    }
+
+}
