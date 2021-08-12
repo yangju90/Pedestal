1481f339546ff7adb175ffcd489f74922c8dab87
Mat <yangju90@163.com>
2021-08-12 13:26:33 +0800

    Update EhcacheConfig.java

--- employee-service/src/main/java/indi/mat/work/project/config/EhcacheConfig.java
+++ employee-service/src/main/java/indi/mat/work/project/config/EhcacheConfig.java
@@ -1,49 +1,92 @@
 package indi.mat.work.project.config;
 
-import indi.mat.work.project.cache.EhCacheManagerFactoryBean;
+import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableCaching
 public class EhcacheConfig extends CachingConfigurerSupport {
 
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
-        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
-        String s = "<ehcache>\n" +
-                "\n" +
-                "    <defaultCache\n" +
-                "            maxElementsInMemory=\"10000\"\n" +
-                "            eternal=\"false\"\n" +
-                "            timeToIdleSeconds=\"3600\"\n" +
-                "            timeToLiveSeconds=\"0\"\n" +
-                "            overflowToDisk=\"false\"\n" +
-                "            diskPersistent=\"false\"\n" +
-                "            diskExpiryThreadIntervalSeconds=\"120\"/>\n" +
-                "\n" +
-                "    <cache\n" +
-                "            name=\"user\"\n" +
-                "            maxEntriesLocalHeap=\"2000\"\n" +
-                "            eternal=\"false\"\n" +
-                "            timeToIdleSeconds=\"3600\"\n" +
-                "            timeToLiveSeconds=\"0\"\n" +
-                "            overflowToDisk=\"false\"\n" +
-                "            statistics=\"true\">\n" +
-                "    </cache>\n" +
-                "\n" +
-                "</ehcache>";
-        ehCacheManagerFactoryBean.setConfigLocation(s);
-//        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("/ehcache.xml"));
-        return ehCacheManagerFactoryBean;
+    public net.sf.ehcache.CacheManager ehCacheManager() {
+        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+        config.setName("EhCache");
+        config.setDynamicConfig(false);
+
+        config.defaultCache(defaultCache());
+        config.addCache(cacheDefine("systemMenu"));
+        config.addCache(cacheDefine("role"));
+        return net.sf.ehcache.CacheManager.newInstance(config);
     }
 
-    @Bean
-    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean ehCacheManagerFactoryBean) {
-        return new EhCacheCacheManager(ehCacheManagerFactoryBean.getObject());
+    private CacheConfiguration defaultCache() {
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.eternal(false);
+        cacheConfiguration.setMaxEntriesLocalHeap(1000);
+        cacheConfiguration.overflowToOffHeap(false);
+        cacheConfiguration.setDiskPersistent(false);
+        cacheConfiguration.setTimeToIdleSeconds(600);
+        cacheConfiguration.timeToLiveSeconds(0);
+        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
+        return cacheConfiguration;
     }
+
+    private CacheConfiguration cacheDefine(String cacheName) {
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.setName(cacheName);
+        cacheConfiguration.eternal(false);
+        cacheConfiguration.setMaxEntriesLocalHeap(1000);
+        cacheConfiguration.setOverflowToDisk(false);
+        cacheConfiguration.setDiskSpoolBufferSizeMB(500);
+        cacheConfiguration.diskExpiryThreadIntervalSeconds(120);
+        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
+        cacheConfiguration.setTimeToIdleSeconds(0);
+        cacheConfiguration.timeToLiveSeconds(600);
+        return cacheConfiguration;
+    }
+
+
+
+
+    @Override
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheManager());
+    }
+
+    @Override
+    public CacheResolver cacheResolver() {
+        return null;
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new SimpleKeyGenerator();
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return null;
+    }
+
+//    @Bean
+//    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean(){
+//        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+//        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("/ehcache.xml"));
+//        return ehCacheManagerFactoryBean;
+//    }
+//
+//    @Bean
+//    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean ehCacheManagerFactoryBean){
+//        return new EhCacheCacheManager(ehCacheManagerFactoryBean.getObject());
+//    }
 }
