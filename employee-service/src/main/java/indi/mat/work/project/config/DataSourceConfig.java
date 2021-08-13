c20de2ec316465b05805f69579135d17e61215f7
yangju90 <yangju90@163.com>
2021-08-13 22:44:30 +0800

    多源配置

--- employee-service/src/main/java/indi/mat/work/project/config/DataSourceConfig.java
+++ employee-service/src/main/java/indi/mat/work/project/config/DataSourceConfig.java
@@ -1,4 +1,9 @@
+package indi.mat.work.project.config;
+
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class DataSourceConfig {
-   // https://www.cnblogs.com/cjsblog/p/9712457.html
+    // https://www.cnblogs.com/cjsblog/p/9712457.html
+    // https://blog.csdn.net/qq_38167579/article/details/86556191
 }
