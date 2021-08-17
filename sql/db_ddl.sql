b0161f86718ab4dad90965ec8e1411ed2cbd4e61
yangju90 <yangju90@163.com>
2021-08-18 00:46:18 +0800

    项目调整Ok

--- sql/db_ddl.sql
+++ sql/db_ddl.sql
@@ -1,13 +1,22 @@
 -- create database if not exists employee default charset utf8mb4 collate utf8mb4_general_ci;
 -- use employee;
 
-create table if not exists `user`(
+create table if not exists `user_1`(
     `id` BIGINT(20) NOT NULL COMMENT '主键ID',
     `name` VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
     `age` INT(11) NULL DEFAULT NULL COMMENT '年龄',
     `email` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
     PRIMARY KEY (`id`)
 );
+
+create table if not exists `user_2`(
+   `id` BIGINT(20) NOT NULL COMMENT '主键ID',
+   `name` VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+   `age` INT(11) NULL DEFAULT NULL COMMENT '年龄',
+   `email` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
+   PRIMARY KEY (`id`)
+);
+
 
 create table if not exists `indi_account_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '逻辑主键',
