4e1ce105c10202a22fe9b819c69fabe1b0d0801b
Mat <yangju90@163.com>
2021-08-18 14:56:23 +0800

    Update DataSourceConfig.java

--- employee-service/src/main/java/indi/mat/work/project/config/DataSourceConfig.java
+++ employee-service/src/main/java/indi/mat/work/project/config/DataSourceConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
-    //https://www.cnblogs.com/taiyonghai/p/9284748.html
+    // AbstractRoutingDataSource 
+    // https://www.cnblogs.com/taiyonghai/p/9284748.html
     // https://www.cnblogs.com/cjsblog/p/9712457.html
     // https://blog.csdn.net/qq_38167579/article/details/86556191
 }
