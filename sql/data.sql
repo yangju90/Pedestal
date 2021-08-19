3a206c3e7e79ef69b923d966cae4e9435bbb32c2
Mat <yangju90@163.com>
2021-08-19 16:39:14 +0800

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,6 @@
 -- https://www.cnblogs.com/klvchen/p/10137117.html 执行优化
+-- limit 优化  https://www.cnblogs.com/weixiaotao/p/10646666.html
+--             https://blog.csdn.net/qq_36276335/article/details/73824243
 
 INSERT INTO user (id, name, age, email) VALUES (1, 'Jone', 18, 'aaaatest1@baomidou.com');
 INSERT INTO user (id, name, age, email) VALUES (2, 'Jack', 20, 'aaaatest2@baomidou.com');
