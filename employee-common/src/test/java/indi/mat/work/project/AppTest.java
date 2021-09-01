737055f9a331b009f7aab36dab7c0f42ea0fbdd8
yangju90 <yangju90@163.com>
2021-09-01 23:13:39 +0800

    修正并提交

--- employee-common/src/test/java/indi/mat/work/project/AppTest.java
+++ employee-common/src/test/java/indi/mat/work/project/AppTest.java
@@ -1,25 +1,16 @@
 package indi.mat.work.project;
 
-import indi.mat.work.project.model.system.SystemMenu;
-import indi.mat.work.project.TestModelModule;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        SystemMenu systemMenu = TestModelModule.getSystemMenu();
-        System.out.println(systemMenu);
-        System.out.println(systemMenu.getHerf());
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+
     }
 }
