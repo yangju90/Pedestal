8513c1486fd5a58c7fb2e1b7b79df47735e6a00a
Mat <yangju90@163.com>
2021-09-04 17:19:30 +0800

    Create CrosConfig.java

---
+++ employee-service-api/src/main/java/indi/mat/work/project/config/CrosConfig.java
@@ -0,0 +1,18 @@
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
