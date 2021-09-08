e6f840dc5cc1c5749c5f9b97de09dc15ca5f119f
Mat <yangju90@163.com>
2021-09-08 17:50:10 +0800

    Create BaseController.java

---
+++ employee-service-api/src/main/java/indi/mat/work/project/controller/BaseController.java
@@ -0,0 +1,17 @@
+public abstract class BaseController {
+    public Response<Void> SUCCESS() {
+        return Response.SUCCESS();
+    }
+
+    public <T> Response<T> SUCCESS(T data) {
+        return Response.SUCCESS(data);
+    }
+
+    public <T> Response<T> SUCCESS(T data, String message) {
+        return Response.SUCCESS(data, message);
+    }
+
+    public Response<Void> FAIL(String message) {
+        return Response.FAIL(message);
+    }
+}
