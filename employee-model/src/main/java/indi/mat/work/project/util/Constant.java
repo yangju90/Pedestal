ea0c9d0bb289529beaad7d608ba66d7247db608f
Mat <yangju90@163.com>
2021-09-10 09:32:27 +0800

    Update Constant.java

--- employee-model/src/main/java/indi/mat/work/project/util/Constant.java
+++ employee-model/src/main/java/indi/mat/work/project/util/Constant.java
@@ -3,4 +3,6 @@ package indi.mat.work.project.util;
 public class Constant {
     public static final String LOGIN_USER_IN_REQUEST = "login_info";
     public static final String JWT_HEADER = "jwt_secret";
+    public static final String DEFAULT_USER = "employee";
+    public static final String DEFAULT_USER_EMAIL = "employee@163.com";
 }
