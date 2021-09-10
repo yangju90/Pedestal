724c555d9b7ad45c9602ab71c1ff5f9b9b0fe9ca
Mat <yangju90@163.com>
2021-09-10 16:00:12 +0800

    Create SSLUtils.java
    处理java请求HTTPS请求时，出现的PKIX问题， 调试状态下没有证书

---
+++ employee-service/src/main/java/indi/mat/work/project/util/SSLUtils.java
@@ -0,0 +1,48 @@
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+
+/**
+ * @author Mat
+ * @version 1: SSLUtils, v 0.1 2021-09-10 2:32 PM Yang
+ */
+public class SSLUtils {
+
+
+    /**
+     * use: RestTemplate restTemplate = new RestTemplate(generateHttpIgnoreSSLFactory());
+     */
+    public HttpComponentsClientHttpRequestFactory generateHttpIgnoreSSLFactory() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = ((x509Certificates, authType) -> true);
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        SSLConnectionSocketFactory connectionSocketFactory = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+        httpClientBuilder.setSSLSocketFactory(connectionSocketFactory);
+        CloseableHttpClient httpClient = httpClientBuilder.build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(httpClient);
+        return factory;
+    }
+
+    /**
+     * use: HttpClient 使用方法
+     */
+    public static HttpClient getHttpClientIgnoreSSL() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        SSLContext sslContext = SSLContextBuilder.create().loadTrustMaterial((x, y) -> true).build();
+        return HttpClientBuilder.create().setSSLContext(sslContext).setSSLHostnameVerifier((x, y) -> true).build();
+    }
+}
