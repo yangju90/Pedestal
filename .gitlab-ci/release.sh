fa3b38fa77f6d28b0a5fa0df549456f7bb02053e
Mat <yangju90@163.com>
2021-11-27 16:32:11 +0800

    Create release.sh

---
+++ .gitlab-ci/release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+IMAGE_NAME=${DEV_HARBOR}/${HARBOR_PROJECT}/tms/logistics-pda-api:${CI_COMMIT_TAG}
+PRD_IMAGE_NAME=${PRD_HARBOR}/${HARBOR_PROJECT}/tms/logistics-pda-api:${CI_COMMIT_TAG}
+
+docker tag ${IMAGE_NAME} ${PRD_IMAGE_NAME}
+docker login -u ${LOGISTICS_PRD_HARBOR_USERNAME} -p ${LOGISTICS_PRD_HARBOR_PASSWORD} ${PRD_HARBOR}
+docker push ${PRD_IMAGE_NAME}
