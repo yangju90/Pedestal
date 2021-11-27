09908949ae6249678d35f41cca40d3d904d3b6cb
Mat <yangju90@163.com>
2021-11-27 16:48:07 +0800

    Update docker-entrypoint.sh

--- employee-service-api/docker/docker-entrypoint.sh
+++ employee-service-api/docker/docker-entrypoint.sh
@@ -1,1 +1,50 @@
+#!/bin/sh
 
+location=${LOCATION}
+
+option=
+for arg in "$@"
+do
+    key=${arg%%=*}
+    value=${arg#*=}
+    case $key in
+        --location)
+            location=$value
+        ;;
+        *)
+            option="$option $key=$value"
+        ;;
+    esac
+done
+
+if [ ! "${location}" ]; then
+    echo "location not set!"
+    exit 1
+fi
+
+echo "LOCATION: ${location}"
+echo "TZ: ${TZ}"
+
+HOST_UTL=""
+
+# 根据不同服务器获取service config
+
+if [ ${location} == 'gdev' ]; then
+  HOST_UTL="apis-dev.xx.com"
+elif [ ${location} == 'gqc' ]; then
+  HOST_UTL="apis-gqc.xx.com"
+elif [ ${location} == 'sandbox' ]; then
+  HOST_UTL="sandboxapis.xx.com"
+elif [[ ${location} == prd* ]]; then
+  HOST_UTL="apis.xx.com"
+else
+  echo "location is error !"
+fi
+
+echo "https://${HOST_UTL}/keeper/v2/shipping-api/application/_rawdata"
+wget -O ./application.yml  "https://${HOST_UTL}/keeper/v2/logistics-pda-api/application/_rawdata"
+
+
+echo "JAVA_OPTS: ${JAVA_OPTS}"
+
+java ${JAVA_OPTS} -Dlogging.config="file:./logback.xml" -jar pda-api.jar${option}
