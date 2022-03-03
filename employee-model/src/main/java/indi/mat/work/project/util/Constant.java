bc6d8aba8196871e69f737cdc9d99781e5735e07
Mat <yangju90@163.com>
2022-03-03 16:49:32 +0800

    Update Constant.java

--- employee-model/src/main/java/indi/mat/work/project/util/Constant.java
+++ employee-model/src/main/java/indi/mat/work/project/util/Constant.java
@@ -5,4 +5,10 @@ public class Constant {
     public static final String JWT_HEADER = "jwt_secret";
     public static final String DEFAULT_USER = "employee";
     public static final String DEFAULT_USER_EMAIL = "employee@163.com";
+    
+    public static final String PID_HEADER = "x-neplatform-pid";
+    /** warehouse number  */
+    public static final String WAREHOUSE_NO_HEADER = "x-warehouse-no";
+    /** pda version code  */
+    public static final String VERSION_CODE = "x-version-code";
 }
