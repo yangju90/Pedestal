f83b00b6d58830a099db6f5cca9244c1c20c54a9
Mat <yangju90@163.com>
2022-03-04 16:13:35 +0800

    Update Constant.java

--- employee-model/src/main/java/indi/mat/work/project/util/Constant.java
+++ employee-model/src/main/java/indi/mat/work/project/util/Constant.java
@@ -11,4 +11,9 @@ public class Constant {
     public static final String WAREHOUSE_NO_HEADER = "x-warehouse-no";
     /** pda version code  */
     public static final String VERSION_CODE = "x-version-code";
+    
+    /** TraceId */
+    public static final String TID = "x-gateway-tid";
+    /** TraceSpanId */
+    public static final String SID = "x-gateway-sid";
 }
