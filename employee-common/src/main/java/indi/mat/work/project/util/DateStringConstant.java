dd4f88e7993cf5331b3f254d58f5536567957947
Mat <yangju90@163.com>
2022-04-02 18:24:56 +0800

    Create DateStringConstant.java

---
+++ employee-common/src/main/java/indi/mat/work/project/util/DateStringConstant.java
@@ -0,0 +1,39 @@
+
+
+public class DateStringConstant {
+
+    public static final String time = "yyyy-MM-dd HH:mm:ss";
+
+    // Token: 0x04000018 RID: 24
+    public static final String fulltime = "yyyy-MM-dd HH:mm:ss.SSSS";
+
+    // Token: 0x04000019 RID: 25
+    public static final String date = "yyyy-MM-dd";
+
+    // Token: 0x0400001A RID: 26
+    public static final String yearMonth = "yyyy-MM";
+
+    // Token: 0x0400001B RID: 27
+    public static final String dateDigitOnly = "yyyyMMdd";
+
+    // Token: 0x0400001C RID: 28
+    public static final String timeDigitOnly = "yyyyMMddhhmmss";
+
+    // Token: 0x0400001D RID: 29
+    public static final String minuteTimeDigitOnly = "yyyyMMddhhmm";
+
+    // Token: 0x0400001E RID: 30
+    public static final String fulltimeDigitOnly = "yyyyMMddhhmmSSSS";
+
+    // Token: 0x0400001F RID: 31
+    public static final String day2Digit = "dd";
+
+    // Token: 0x04000020 RID: 32
+    public static final String month2Digit = "MM";
+
+    // Token: 0x04000021 RID: 33
+    public static final String year4Digit = "yyyy";
+
+    // Token: 0x04000022 RID: 34
+    public static final String normalTimeOnly = "HH:mm:ss";
+}
