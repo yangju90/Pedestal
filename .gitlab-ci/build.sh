470c99263c8782eff55ffa787437d6e63eb3911f
yangju90 <yangju90@163.com>
2022-07-23 00:54:14 +0800

    gitlab config

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -11,13 +11,13 @@ fi
 # sonar check
 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN}
 
-cd logistics-pda-api-rest
+cd employee-service-api
 # build docker image
-IMAGE_NAME=${DEV_HARBOR}/${HARBOR_PROJECT}/tms/logistics-pda-api:${CI_COMMIT_TAG}
+IMAGE_NAME=${DEV_HARBOR}/${HARBOR_PROJECT}/library/employee-service-api:${CI_COMMIT_TAG}
 OLD_IMAGE_ID=`docker images -q ${IMAGE_NAME}`
 docker build --pull -t ${IMAGE_NAME} .
 # push docker image to dev harbor
-docker login -u ${LOGISTICS_DEV_HARBOR_USERNAME} -p ${LOGISTICS_DEV_HARBOR_PASSWORD} ${DEV_HARBOR}
+docker login -u ${DEV_HARBOR_USERNAME} -p ${DEV_HARBOR_PASSWORD} ${DEV_HARBOR}
 docker push ${IMAGE_NAME}
 
 # clean old docker image
--- .gitlab-ci/release.sh
+++ .gitlab-ci/release.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-IMAGE_NAME=${DEV_HARBOR}/${HARBOR_PROJECT}/tms/logistics-pda-api:${CI_COMMIT_TAG}
-PRD_IMAGE_NAME=${PRD_HARBOR}/${HARBOR_PROJECT}/tms/logistics-pda-api:${CI_COMMIT_TAG}
+IMAGE_NAME=${DEV_HARBOR}/${HARBOR_PROJECT}/library/employee-service-api:${CI_COMMIT_TAG}
+PRD_IMAGE_NAME=${PRD_HARBOR}/${HARBOR_PROJECT}/library/employee-service-api:${CI_COMMIT_TAG}
 
 docker tag ${IMAGE_NAME} ${PRD_IMAGE_NAME}
-docker login -u ${LOGISTICS_PRD_HARBOR_USERNAME} -p ${LOGISTICS_PRD_HARBOR_PASSWORD} ${PRD_HARBOR}
+docker login -u ${PRD_HARBOR_USERNAME} -p ${PRD_HARBOR_PASSWORD} ${PRD_HARBOR}
 docker push ${PRD_IMAGE_NAME}
