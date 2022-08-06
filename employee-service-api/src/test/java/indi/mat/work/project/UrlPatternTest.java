7ad0654743e70331bf1807a71e6931fff15e4a4a
yangju90 <yangju90@163.com>
2022-08-06 21:53:33 +0800

    Spring Url Pattern Example

---
+++ employee-service-api/src/test/java/indi/mat/work/project/UrlPatternTest.java
@@ -0,0 +1,20 @@
+package indi.mat.work.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.AntPathMatcher;
+
+/**
+ * @author Mat
+ * @version : UrlPatternTest, v 0.1 2022-08-06 21:17 Yang
+ */
+
+public class UrlPatternTest {
+
+    @Test
+    public void test(){
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+        System.out.println(antPathMatcher.matchStart("/api/user/{id}", "/api/user/1"));
+        System.out.println(antPathMatcher.matchStart("/api/user", "/api/user/1"));
+    }
+}
